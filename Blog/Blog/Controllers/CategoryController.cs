61e287b3dc40a8ff02048a332c7fd16911128c3c
Cleberton Carvalho <clebertonfgc@gmail.com>
2023-10-17 21:46:14 -0300

    add CategoryController

--- Blog/Blog/Controllers/CategoryController.cs
+++ Blog/Blog/Controllers/CategoryController.cs
@@ -1,16 +1,80 @@
 ﻿using Blog.Data;
+using Blog.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Blog.Controllers
 {
     [ApiController]
     public class CategoryController : ControllerBase
     {
-        [HttpGet("categories")]
-        public IActionResult Get([FromServices] BlogDataContext context)
+        [HttpGet("v1/categories")]
+        public async Task<IActionResult> GetAsync([FromServices] BlogDataContext context)
         {
-            var categories = context.Categories.ToList();
+            var categories = await context.Categories.ToListAsync();
             return Ok(categories);
+        }
+
+        [HttpGet("v1/categories/{id:int}")]
+        public async Task<IActionResult> GetByIdAsync([FromServices] BlogDataContext context,
+                                                      [FromRoute] int id)
+        {
+            var category = await context.Categories.FirstOrDefaultAsync(x => x.Id == id);
+
+            return category is not null ? Ok(category) : NotFound();
+        }
+
+        [HttpPost("v1/categories")]
+        public async Task<IActionResult> PostAsync([FromServices] BlogDataContext context, 
+                                                   [FromBody] Category model)
+        {
+            try
+            {
+                await context.Categories.AddAsync(model);
+                await context.SaveChangesAsync();
+
+                return Created($"v1/categories/{model.Id}", model);
+            }
+            catch (DbUpdateException ex)
+            {
+                return StatusCode(500, $"05EX09 - {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(500, $"05EX10 - {ex.Message}");
+            }
+
+        }
+
+        [HttpPut("v1/categories/{id:int}")]
+        public async Task<IActionResult> PutAsync([FromServices] BlogDataContext context,
+                                                   [FromRoute] int id,
+                                                   [FromBody] Category model)
+        {
+            var category = await context.Categories.FirstOrDefaultAsync(x => x.Id == id);
+            if (category is null) return NotFound();
+
+            category.Name = model.Name;
+            category.Slug = model.Slug;
+
+            context.Categories.Update(category);
+            await context.SaveChangesAsync();
+
+            return Ok(model);
+        }
+
+        [HttpDelete("v1/categories/{id:int}")]
+        public async Task<IActionResult> PutAsync([FromServices] BlogDataContext context,
+                                                   [FromRoute] int id)
+        {
+            var category = await context.Categories.FirstOrDefaultAsync(x => x.Id == id);
+            if (category is null) return NotFound();
+
+            context.Categories.Remove(category);
+            await context.SaveChangesAsync();
+
+            return Ok(category);
         }
     }
 }
--- Blog/Blog/Models/Tag.cs
+++ Blog/Blog/Models/Tag.cs
@@ -5,9 +5,9 @@ namespace Blog.Models
     public class Tag
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Slug { get; set; }
+        public string? Name { get; set; }
+        public string? Slug { get; set; }
 
-        public List<Post> Posts { get; set; }
+        public List<Post>? Posts { get; set; }
     }
 }
